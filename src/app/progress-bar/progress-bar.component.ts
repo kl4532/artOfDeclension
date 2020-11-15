25b388ff10db2432551b15e100516674e50685e2
kl4532 <kornellukasiak@gmail.com>
2020-11-15 22:13:21 +0100

    added progress-bar component

---
+++ src/app/progress-bar/progress-bar.component.ts
@@ -0,0 +1,52 @@
+import {Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-progress-bar',
+  templateUrl: './progress-bar.component.html',
+  styleUrls: ['./progress-bar.component.scss']
+})
+export class ProgressBarComponent implements OnInit{
+
+  constructor(private elem: ElementRef) { }
+  inner: HTMLElement;
+  interval: any;
+  time: number;
+  @Input() initTime: number;
+  @Output() finished = new EventEmitter<boolean>();
+
+  @Input()
+  public set start(val: boolean) {
+    if (val) {
+      console.log('progressStarted', val);
+      this.startProgress();
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+  process() {
+    if( this.time <= 0){
+      this.inner.style.width = '0%';
+      clearInterval(this.interval);
+      this.finished.emit(true);
+      return;
+    }
+
+    this.time  = this.time - 0.1;
+    const procent = this.time * 100 /this.initTime;
+    this.setProgress(procent);
+  }
+
+  startProgress() {
+    this.inner = this.elem.nativeElement.querySelector('.inner');
+    this.time = this.initTime;
+    this.setProgress(100);
+    this.interval = setInterval( () => this.process(),100);
+  }
+
+  setProgress(proc: number) {
+    this.inner.style.width = proc + "%";
+  }
+
+}
