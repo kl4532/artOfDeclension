169bf99593e4aece1623ae1f29eb522080ee702f
kl4532 <kornellukasiak@gmail.com>
2020-11-15 22:13:42 +0100

    added progress-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import {FormsModule} from "@angular/forms";
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgressBarComponent,
   ],
   imports: [
     BrowserModule,
